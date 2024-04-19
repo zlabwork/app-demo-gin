21754b6a987c84fa6ec8f2358719a53857d6e522
Joe <joe@xxtime.com>
2024-04-19 16:56:05 +0800

    update & headers

--- internal/listener/before_listener.go
+++ internal/listener/before_listener.go
@@ -10,6 +10,14 @@ func Before() gin.HandlerFunc {
 		trace := uuid.New().String()
 		// c.Set("traceId", trace)
 		c.Header("X-Request-Id", trace)
+
+		c.Header("X-Frame-Options", "SAMEORIGIN")
+		c.Header("X-Content-Type-Options", "nosniff")
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+		c.Header("Referrer-Policy", "origin")
+		// c.Header("Content-Security-Policy", "default-src 'self';")
+
 		c.Next()
 	}
 }
