0e1324b3ba0f6309c9445bf888d00a87502acba8
Joe <joe@xxtime.com>
2024-05-11 11:10:36 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,18 @@
 env=".env"
 cfg="config/app.yaml"
 
+if [[ ! -f "config" ]]; then
+  mkdir -p "config"
+fi
+
+if [[ ! -f "storage/logs" ]]; then
+  mkdir -p "storage/logs"
+fi
+
+if [[ ! -f "storage/data" ]]; then
+  mkdir -p "storage/data"
+fi
+
 if [[ ! -f $env ]]; then
   echo "$env is not exits"
   exit
