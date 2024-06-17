8f02dfbcdf0a71c71ad33ff553dfb311f86dad70
Joe <joe@xxtime.com>
2024-06-17 16:17:18 +0800

    environment

--- run.sh
+++ run.sh
@@ -1,30 +1,60 @@
 #!/bin/bash
 
-env=".env"
-cfg="config/app.yaml"
+env_file=".env"
+cfg_file="config/app.yaml"
 
-if [[ ! -f "config" ]]; then
-  mkdir -p "config"
-fi
+# build directory
+function check_dir() {
+    if [[ ! -f "config" ]]; then
+      mkdir -p "config"
+    fi
 
-if [[ ! -f "storage/logs" ]]; then
-  mkdir -p "storage/logs"
-fi
+    if [[ ! -f "storage/logs" ]]; then
+      mkdir -p "storage/logs"
+    fi
 
-if [[ ! -f "storage/data" ]]; then
-  mkdir -p "storage/data"
-fi
+    if [[ ! -f "storage/data" ]]; then
+      mkdir -p "storage/data"
+    fi
+}
 
-if [[ ! -f $env ]]; then
-  echo "$env is not exits"
-  exit
-fi
+# check environment
+function export_env() {
+    if [[ ! -f $env_file ]]; then
+      echo "$env_file is not exits"
+      exit
+    fi
 
-if [[ ! -f $cfg ]]; then
-  echo "$cfg is not exits"
-  exit
-fi
+    if [[ ! -f $cfg_file ]]; then
+      echo "$cfg_file is not exits"
+      exit
+    fi
 
-export $(cat $env | grep -v "#")
+    # export env
+    export $(cat $env_file | grep -v "#")
 
-go run main.go
+    # overwrite env
+    if [ -z "$APP_ENV" ]; then
+        echo "use .env file as default"
+    elif [[ "$APP_ENV" != prod* ]]; then
+        env_file=".env.$APP_ENV"
+        if [[ ! -f $env_file ]]; then
+          echo "$env_file is not exits"
+          exit
+        fi
+        echo "use $env_file file"
+        export $(cat $env_file | grep -v "#")
+    fi
+}
+
+function run_app() {
+    go run main.go
+}
+
+function main() {
+    check_dir
+    export_env
+    run_app
+}
+
+main
